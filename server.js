91e94d2daf0fdccafe5449f2ee5d042fd276ce81
Elliot Trejo <elliot.trejo.tcu@gmail.com>
2019-12-16 15:02:57 -0600

    Updated listening to port 8000.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 // =============================================================
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 
 // Sets up the Express App
 // =============================================================
@@ -11,3 +12,21 @@ const PORT = 8000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/notes", function(req, res) {
+  // res.send("Welcome to the Star Wars Page!")
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+// Basic route that sends the user first to the AJAX Page
+app.get("*", function(req, res) {
+  // res.send("Welcome to the Star Wars Page!")
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
